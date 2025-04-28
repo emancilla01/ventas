916dd4416467417d09f4551702122230b04dc58f
emancilla01 <emancilla01@gmail.com>
2025-04-28 18:10:17 -0500

    abr 28 updates

--- categorias/ins_act.php
+++ categorias/ins_act.php
@@ -3,10 +3,8 @@
   include_once "../db/db.php";
   $categorias = new db();
   $categorias->conectar();
-
   $id=$_REQUEST['id'];
   $nombre=$_REQUEST['nombre'];
-
   if($id != ""){
     $sql = "UPDATE categorias SET nombre='$nombre' WHERE id='$id'";
     $categorias->actualizar($sql);
